d1029bd9c1dd10b2645f439cf4f5b4b99620e756
Sahidul Islam <jonyahmed19@gmail.com>
2023-04-05 14:35:34 +0600

    logo is updated

--- components/Homepage/index.js
+++ components/Homepage/index.js
@@ -1,24 +1,26 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Homepage = () => {
   return (
     <div>
       <div className="home bg-slate-600 p-6">
         <div className="header flex justify-between items-center">
-          <div className="logo text-white text-2xl font-semibold">EduCare</div>
-          <div className='flex space-x-6 text-white'>
+          <div className="logo text-white text-2xl font-semibold">
+            EduCareLms
+          </div>
+          <div className="flex space-x-6 text-white">
             <div>
-              <Link href='/login'>Login</Link>
+              <Link href="/login">Login</Link>
             </div>
             <div>
-              <Link href='signup'>Register</Link>
+              <Link href="signup">Register</Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
