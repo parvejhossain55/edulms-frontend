87c64ca510bbf60cbad5124cb069e255f38797bd
Kamrul Hasan <991khriad@gmail.com>
2023-04-09 15:35:28 +0600

    change

--- components/Shared/Footer/index.jsx
+++ components/Shared/Footer/index.jsx
@@ -1,11 +1,18 @@
-import Link from "next/link";
 import React from "react";
 
 
 const Footer = () => {
     return (
-        <div>
-          Footer Sections
+        <div className='bg-black-200'>
+            <div className="container-kh py-10 ">
+                <div className="flex flex-col">
+                    <div className="flex"></div>
+                    <div className="flex"></div>
+                    <div className="flex"></div>
+                    <div className="flex"></div>
+                </div>
+            </div>
+
         </div>
     );
 };
