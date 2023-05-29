d4d00513d19ac34dd8add0c6e573db64cf15f46b
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-05-29 21:10:53 +0600

    some style change in programGallery

--- components/Program/ProgramGallery/index.js
+++ components/Program/ProgramGallery/index.js
@@ -13,73 +13,115 @@ const ProgramGallery  = () => {
             <div className="containte-kh px-4 py-8 sm:py-10 md:py-12 lg:py-16">
                 <p className="title-kh">Activities</p>
                 <h1 className='heading-kh my-6 text-[28px] text-[35px] text-[45px]  text-center capitalize'>Our Activities Gallery</h1>
-                <div className="flex flex-col justify-start md:flex-wrap md:flex-row gap-y-7 mt-10">
+                <div className="flex flex-col justify-start md:justify-between md:flex-wrap md:flex-row gap-y-7 mt-10">
 
 
-                    <div className="flex justify-start items-start basis-full md:basis-6/12 lg:basis-4/12 activity-box transition-all">
-                        <div className='relative'>
-                           <Image src={gallery1}
-                                  style={{width:"auto" , height:"auto"}}
-                                  className='w-full rounded-2xl  '
-                                  alt='activities gallery'/>
-                            <div className='flex justify-center items-center rounded-2xl absolute top-0 left-0 w-full h-full transition-all duration-500 ease-in opacity-0 hover:opacity-100 translate-y-5 hover:translate-y-0 '>
-                                <Link href='#' className=' font-bold text-4xl text-white '><BiSearchAlt/></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start items-start basis-full md:basis-6/12 lg:basis-4/12 activity-box transition-all">
-                        <div className=' relative'>
-                           <Image src={gallery2}
-                                  style={{width:"auto" , height:"auto"}}
-                                  className='w-full rounded-2xl  '
-                                  alt='activities gallery'/>
-                            <div className='flex justify-center items-center rounded-2xl absolute top-0 left-0 w-full h-full transition-all duration-500 ease-in opacity-0 hover:opacity-100 translate-y-5 hover:translate-y-0 '>
-                                <Link href='#' className=' font-bold text-4xl text-white '><BiSearchAlt/></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start items-start basis-full md:basis-6/12 lg:basis-4/12 activity-box transition-all">
-                        <div className=' relative'>
-                            <Image src={gallery3}
-                                   style={{width:"auto" , height:"auto"}}
-                                   className='w-full rounded-2xl  '
-                                   alt='activities gallery'/>
-                            <div className='flex justify-center items-center rounded-2xl absolute top-0 left-0 w-full h-full transition-all duration-500 ease-in opacity-0 hover:opacity-100 translate-y-5 hover:translate-y-0 '>
-                                <Link href='#' className=' font-bold text-4xl text-white '><BiSearchAlt/></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start items-start basis-full md:basis-6/12 lg:basis-4/12 activity-box transition-all">
-                        <div className=' relative'>
-                            <Image src={gallery3}
-                                   style={{width:"auto" , height:"auto"}}
-                                   className='w-full rounded-2xl  '
-                                   alt='activities gallery'/>
-                            <div className='flex justify-center items-center rounded-2xl absolute top-0 left-0 w-full h-full transition-all duration-500 ease-in opacity-0 hover:opacity-100 translate-y-5 hover:translate-y-0 '>
-                                <Link href='#' className=' font-bold text-4xl text-white '><BiSearchAlt/></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start items-start basis-full md:basis-6/12 lg:basis-4/12 activity-box transition-all">
-                        <div className=' relative'>
-                            <Image src={gallery2}
-                                   style={{width:"auto" , height:"auto"}}
-                                   className='w-full rounded-2xl  '
-                                   alt='activities gallery'/>
-                            <div className='flex justify-center items-center rounded-2xl absolute top-0 left-0 w-full h-full transition-all duration-500 ease-in opacity-0 hover:opacity-100 translate-y-5 hover:translate-y-0 '>
-                                <Link href='#' className=' font-bold text-4xl text-white '><BiSearchAlt/></Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start items-start basis-full md:basis-6/12 lg:basis-4/12 activity-box transition-all">
-                        <div className=' relative'>
+                    <div className="flex justify-center items-center basis-full md:basis-[48%] lg:basis-[32%] ">
+                        <div className='relative w-full'>
                             <Image src={gallery1}
                                    style={{width:"auto" , height:"auto"}}
-                                   className='w-full rounded-2xl  '
+                                   className='w-full rounded-2xl '
                                    alt='activities gallery'/>
-                            <div className='flex justify-center items-center rounded-2xl absolute top-0 left-0 w-full h-full transition-all duration-500 ease-in opacity-0 hover:opacity-100 translate-y-5 hover:translate-y-0 '>
-                                <Link href='#' className=' font-bold text-4xl text-white '><BiSearchAlt/></Link>
+                            <div className='w-full h-full bg-black bg-opacity-0 hover:bg-opacity-[.35] absolute top-0 left-0 rounded-2xl transition-all duration-500 ease-in  '>
+                                <Link href='#' className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full font-bold text-4xl text-white transition-all duration-500 ease-in translate-y-5 hover:translate-y-0  '><BiSearchAlt/>
+                                </Link>
                             </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center basis-full md:basis-[48%] lg:basis-[32%] ">
+                        <div className='relative w-full'>
+                            <Image src={gallery2}
+                                   style={{width:"auto" , height:"auto"}}
+                                   className='w-full rounded-2xl '
+                                   alt='activities gallery'/>
+                            <div className='w-full h-full bg-black bg-opacity-0 hover:bg-opacity-[.35] absolute top-0 left-0 rounded-2xl transition-all duration-500 ease-in  '>
+                                <Link href='#' className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full font-bold text-4xl text-white transition-all duration-500 ease-in translate-y-5 hover:translate-y-0  '><BiSearchAlt/>
+                                </Link>
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center basis-full md:basis-[48%] lg:basis-[32%] ">
+                        <div className='relative w-full'>
+                            <Image src={gallery3}
+                                   style={{width:"auto" , height:"auto"}}
+                                   className='w-full rounded-2xl '
+                                   alt='activities gallery'/>
+                            <div className='w-full h-full bg-black bg-opacity-0 hover:bg-opacity-[.35] absolute top-0 left-0 rounded-2xl transition-all duration-500 ease-in  '>
+                                <Link href='#' className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full font-bold text-4xl text-white transition-all duration-500 ease-in translate-y-5 hover:translate-y-0  '><BiSearchAlt/>
+                                </Link>
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center basis-full md:basis-[48%] lg:basis-[32%] ">
+                        <div className='relative w-full'>
+                            <Image src={gallery2}
+                                   style={{width:"auto" , height:"auto"}}
+                                   className='w-full rounded-2xl '
+                                   alt='activities gallery'/>
+                            <div className='w-full h-full bg-black bg-opacity-0 hover:bg-opacity-[.35] absolute top-0 left-0 rounded-2xl transition-all duration-500 ease-in  '>
+                                <Link href='#' className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full font-bold text-4xl text-white transition-all duration-500 ease-in translate-y-5 hover:translate-y-0  '><BiSearchAlt/>
+                                </Link>
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center basis-full md:basis-[48%] lg:basis-[32%] ">
+                        <div className='relative w-full'>
+                            <Image src={gallery3}
+                                   style={{width:"auto" , height:"auto"}}
+                                   className='w-full rounded-2xl '
+                                   alt='activities gallery'/>
+                            <div className='w-full h-full bg-black bg-opacity-0 hover:bg-opacity-[.35] absolute top-0 left-0 rounded-2xl transition-all duration-500 ease-in  '>
+                                <Link href='#' className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full font-bold text-4xl text-white transition-all duration-500 ease-in translate-y-5 hover:translate-y-0  '><BiSearchAlt/>
+                                </Link>
+                            </div>
+
+
+
+
+
+
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center basis-full md:basis-[48%] lg:basis-[32%] ">
+                        <div className='relative w-full'>
+                            <Image src={gallery1}
+                                   style={{width:"auto" , height:"auto"}}
+                                   className='w-full rounded-2xl '
+                                   alt='activities gallery'/>
+                            <div className='w-full h-full bg-black bg-opacity-0 hover:bg-opacity-[.35] absolute top-0 left-0 rounded-2xl transition-all duration-500 ease-in  '>
+                                <Link href='#' className='opacity-0 hover:opacity-100 flex justify-center items-center w-full h-full font-bold text-4xl text-white transition-all duration-500 ease-in translate-y-5 hover:translate-y-0  '><BiSearchAlt/>
+                                </Link>
+                            </div>
+
+
+
+
+
+
                         </div>
                     </div>
 
