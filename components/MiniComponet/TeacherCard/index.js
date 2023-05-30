19b9c151c65d42d5015e002c3babb9b386edef3d
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-05-30 23:02:38 +0600

    some style change in expartTeacher componect

--- components/MiniComponet/TeacherCard/index.js
+++ components/MiniComponet/TeacherCard/index.js
@@ -17,16 +17,24 @@ const TeacherCard = ({
         <div className="hover-box flex basis-12 md:basis-[48%] lg:basis-[32%] relative">
             <Image src={image} className='w-full rounded-xl'  alt='Expart-taacher'/>
 
-            <div className=" flex flex-col gap-y-4 opacity-0 hover:opacity-100 translate-y-2 hover:translate-y-0 bg-opacity-0 hover:bg-opacity-50 justify-center items-center rounded-xl hover:mb-3
-             bg-black w-full h-full transition ease-in duration-500 absolute right-0 left-0  ">
-                <h2 className='font-bold text-kh-white text-2xl capitalize '>{teacherName}</h2>
-                <span className='text-white font-semibold'>{teacherTitle}</span>
-                <div className="icon flex justify-start items-center gap-5 opacity-100 ">
-                    <Link className='icon-kh' href={`${fbUrl}`}> <FaFacebookF/></Link>
-                    <Link className='icon-kh' href={`${twUrl}`}><BsTwitter/></Link>
-                    <Link className='icon-kh' href={`${googleUrl}`}><FaGooglePlusG/></Link>
+
+            <div className='flex justify-center items-center w-full h-full bg-[#000] bg-opacity-0 hover:bg-opacity-50 absolute right-0 left-0  transition ease-in duration-500  rounded-xl'>
+
+                <div className=" flex flex-col justify-center items-center gap-y-4 opacity-0 hover:opacity-100  bg-transparent
+              w-full h-full transition ease-in duration-500 translate-y-5 hover:translate-y-0 ">
+                    <h2 className='font-bold text-kh-white text-2xl capitalize '>{teacherName}</h2>
+                    <span className='text-white font-semibold'>{teacherTitle}</span>
+                    <div className="icon flex justify-start items-center gap-x-5 opacity-100 ">
+                        <Link className='flex justify-center items-center bg-kh-white w-10 h-10 rounded-full text-yellow-kh hover:bg-yellow-kh hover:text-kh-white transition-all duration-500 ease-in' href={`${fbUrl}`}> <FaFacebookF/></Link>
+                        <Link className='flex justify-center items-center bg-kh-white w-10 h-10 rounded-full text-yellow-kh hover:bg-yellow-kh hover:text-kh-white transition-all duration-500 ease-in' href={`${twUrl}`}><BsTwitter/></Link>
+                        <Link className='flex justify-center items-center bg-kh-white w-10 h-10 rounded-full text-yellow-kh hover:bg-yellow-kh hover:text-kh-white transition-all duration-500 ease-in' href={`${googleUrl}`}><FaGooglePlusG/></Link>
+                    </div>
                 </div>
+
+
             </div>
+
+
         </div>
     );
 };
